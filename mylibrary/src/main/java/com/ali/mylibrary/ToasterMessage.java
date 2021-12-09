14e8f31acf45f66deb38482142d73915f93533f4
aliahmad39 <a.ahmad0315@gmail.com>
2021-12-09 23:11:05 +0500

    update

---
+++ mylibrary/src/main/java/com/ali/mylibrary/ToasterMessage.java
@@ -0,0 +1,20 @@
+package com.ali.mylibrary;
+
+import android.content.Context;
+import android.content.Intent;
+import android.widget.Toast;
+
+public class ToasterMessage {
+
+    public static void s(Context c, String message){
+
+        Toast.makeText(c,message, Toast.LENGTH_SHORT).show();
+
+    }
+
+    public static void start(Context c){
+
+        c.startActivity(new Intent(c , CalcuActivity.class));
+
+    }
+}
