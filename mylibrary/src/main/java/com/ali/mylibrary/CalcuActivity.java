36ef982cbf77f21a396aa72385f6a1f37011764d
aliahmad39 <a.ahmad0315@gmail.com>
2021-12-09 23:28:24 +0500

    second commit

--- mylibrary/src/main/java/com/ali/mylibrary/CalcuActivity.java
+++ mylibrary/src/main/java/com/ali/mylibrary/CalcuActivity.java
@@ -3,6 +3,7 @@ package com.ali.mylibrary;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
@@ -17,7 +18,16 @@ public class CalcuActivity extends AppCompatActivity {
         num2 = findViewById(R.id.et_2);
         check = findViewById(R.id.btn_submit);
 
-
+check.setOnClickListener(new View.OnClickListener() {
+    @Override
+    public void onClick(View view) {
+        int a = Integer.parseInt(num1.getText().toString());
+        int b = Integer.parseInt(num2.getText().toString());
+        int c= a + b;
+        ToasterMessage.display(CalcuActivity.this , c);
+        finish();
+    }
+});
 
 
     }
--- mylibrary/src/main/java/com/ali/mylibrary/ToasterMessage.java
+++ mylibrary/src/main/java/com/ali/mylibrary/ToasterMessage.java
@@ -7,14 +7,14 @@ import android.widget.Toast;
 public class ToasterMessage {
 
     public static void s(Context c, String message){
-
         Toast.makeText(c,message, Toast.LENGTH_SHORT).show();
-
     }
 
     public static void start(Context c){
-
         c.startActivity(new Intent(c , CalcuActivity.class));
 
+    }
+    public static void display(Context c , int result){
+       s(c,"result is:"+result);
     }
 }
