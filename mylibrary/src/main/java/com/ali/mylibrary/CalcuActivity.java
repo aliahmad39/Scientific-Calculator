f7d510d5097063bd1cb21c1c41f1f393826a7d80
aliahmad39 <a.ahmad0315@gmail.com>
2021-12-10 08:55:06 +0500

    designcode

--- mylibrary/src/main/java/com/ali/mylibrary/CalcuActivity.java
+++ mylibrary/src/main/java/com/ali/mylibrary/CalcuActivity.java
@@ -8,27 +8,35 @@ import android.widget.Button;
 import android.widget.EditText;
 
 public class CalcuActivity extends AppCompatActivity {
-    EditText num1 , num2;
+    EditText num1, num2;
     Button check;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calcu);
+        bindViews();
+        clickListeners();
+
+
+    }
+
+    private void clickListeners() {
+        check.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                double a = Double.parseDouble(num1.getText().toString());
+                double b = Double.parseDouble(num2.getText().toString());
+                double c = a + b;
+                ToasterMessage.display(CalcuActivity.this, Integer.parseInt(String.valueOf(c)));
+                finish();
+            }
+        });
+    }
+
+    private void bindViews() {
         num1 = findViewById(R.id.et_1);
         num2 = findViewById(R.id.et_2);
         check = findViewById(R.id.btn_submit);
-
-check.setOnClickListener(new View.OnClickListener() {
-    @Override
-    public void onClick(View view) {
-        int a = Integer.parseInt(num1.getText().toString());
-        int b = Integer.parseInt(num2.getText().toString());
-        int c= a + b;
-        ToasterMessage.display(CalcuActivity.this , c);
-        finish();
-    }
-});
-
-
     }
 }
